3019b97a67d84161a702e91e32f385c93638e9dc
Lanice <172978981+laniceofficial@users.noreply.github.com>
2026-03-01 13:09:08 +0800

    v1.0.1爆改前的保存

--- User/task/task.c
+++ User/task/task.c
@@ -1,29 +1,31 @@
-#include "pid.h"
-#include "main.h"
+#include "task.h"
 #include "collection.h"
 #include "dma.h"
-#include "tim.h"
 #include "gpio.h"
-#include "task.h"
 #include "hrtim.h"
-#include "svpwm.h"
+#include "main.h"
 #include "oled.h"
+#include "pid.h"
+#include "svpwm.h"
+#include "tim.h"
+
 // #include "draw_api.h"
-//PC6--F1，PC8--E1
+// PC6--F1，PC8--E1
 // 开关频率f = 6.8E8 / (2*HRTIM_M_HALF_CNT)
-#define HRTIM_M_HALF_CNT (27200/2)
-#define HRTIM_1per4_CNT (HRTIM_M_HALF_CNT/2)
+#define HRTIM_M_CNT (27200)
+#define HRTIM_M_HALF_CNT (HRTIM_M_CNT / 2)
+#define HRTIM_1per4_CNT (HRTIM_M_CNT / 4)
 #define SIN_ALL_PIONT 1000
-#define SQRT_2_3 0.8165f     // sqrt(2.0/3.0)
-#define PI_2_3 2.0944f       // 2*PI/3
-#define SQRT3_DIV_3 0.57735f // 1.732f/3
-asm(".global _printf_float");
+#define SQRT_2_3 0.8165f      // sqrt(2.0/3.0)
+#define PI_2_3 2.0944f        // 2*PI/3
+#define SQRT3_DIV_3 0.57735f  // 1.732f/3
+asm(".global _printf_float"); // oled使用printf
 
 float M_duty = 0.85f; // 调制比
 
 // todo 测试公式 Uab是否准确计算
-//测试闭环svpwm效果
-//实在不行就测试采集Udc后进行闭环
+// 测试闭环svpwm效果
+// 实在不行就测试采集Udc后进行闭环
 
 static int32_t sin_table1[SIN_ALL_PIONT] = {0};
 // static int32_t sin_table2[SIN_ALL_PIONT] = {0};
@@ -31,7 +33,7 @@ static int32_t sin_table1[SIN_ALL_PIONT] = {0};
 
 PID voltage_PID;
 PID current_PID;
-float L_voltage_ref = 9; //线电压有效值参考值
+float L_voltage_ref = 9;    // 线电压有效值参考值
 float X_voltage_ref = 26.1; // 32*sqrt(2/3) 相电压最大值
 float current_ref = 0;
 float voltage_fdb = 0;
@@ -39,39 +41,37 @@ float current_fdb = 0;
 float add_freq = 0;
 collect_data_t *collect_data = NULL;
 svpwm_t svpwm_v;
- void task_init()
- {
-    //  OLED_Init();
-    //  OLED_Clear();
-    //  OLED_Printf(0, 0, OLED_8X16,  "FREQ:        Hz");
-    //  OLED_Printf(0, 16, OLED_8X16, "A:            V");
-    //  OLED_Printf(0, 32, OLED_8X16, "B:            V");
-    //  OLED_Printf(0, 48, OLED_8X16, "C:            V");
-    //  OLED_Update();
-      sinTab_genarate();
-    //  svpwm_init(&svpwm_v, 0, 50, 10000);
-    //  collect_data = collection_init();
-     pid_init(&voltage_PID, PID_DELTA, V_KP, V_KI, 0, 0.5, 0.57, 0);
-     //  pid_init(&current_PID, PID_DELTA, 0.01, 0, 0, 0.5, 1.2, 0);
-     HAL_HRTIM_WaveformCounterStart(&hhrtim1, HRTIM_TIMERID_MASTER);
-     HAL_HRTIM_WaveformCounterStart(&hhrtim1, HRTIM_TIMERID_TIMER_E);
-     HAL_HRTIM_WaveformCounterStart(&hhrtim1, HRTIM_TIMERID_TIMER_F);
-    //  HAL_HRTIM_WaveformCounterStart(&hhrtim1, HRTIM_TIMERID_TIMER_B);
-     HAL_TIM_Base_Start_IT(&htim6);
-    //  HAL_TIM_Base_Start_IT(&htim5);
-     voltage_PID.ref = L_voltage_ref; // 有效值为15V
- }
+void task_init() {
+  //  OLED_Init();
+  //  OLED_Clear();
+  //  OLED_Printf(0, 0, OLED_8X16,  "FREQ:        Hz");
+  //  OLED_Printf(0, 16, OLED_8X16, "A:            V");
+  //  OLED_Printf(0, 32, OLED_8X16, "B:            V");
+  //  OLED_Printf(0, 48, OLED_8X16, "C:            V");
+  //  OLED_Update();
+  sinTab_genarate();
+  //  svpwm_init(&svpwm_v, 0, 50, 10000);
+  //  collect_data = collection_init();
+  pid_init(&voltage_PID, PID_DELTA, V_KP, V_KI, 0, 0.5, 0.57, 0);
+  //  pid_init(&current_PID, PID_DELTA, 0.01, 0, 0, 0.5, 1.2, 0);
+  HAL_HRTIM_WaveformCounterStart(&hhrtim1, HRTIM_TIMERID_MASTER);
+  HAL_HRTIM_WaveformCounterStart(&hhrtim1, HRTIM_TIMERID_TIMER_E);
+  HAL_HRTIM_WaveformCounterStart(&hhrtim1, HRTIM_TIMERID_TIMER_F);
+  //  HAL_HRTIM_WaveformCounterStart(&hhrtim1, HRTIM_TIMERID_TIMER_B);
+  HAL_TIM_Base_Start_IT(&htim6);
+  //  HAL_TIM_Base_Start_IT(&htim5);
+  voltage_PID.ref = L_voltage_ref; // 有效值为15V
+}
 
-  void sinTab_genarate()
- {
-     for (uint16_t i = 0; i < SIN_ALL_PIONT; i++)
-     {
-         // 每个周期覆盖完整的2π (360°)
-         float angle = 2.0f * PI * i / SIN_ALL_PIONT;
-         sin_table1[i] = (int32_t)(arm_sin_f32(angle) * HRTIM_1per4_CNT);
-        //  sin_table2[i] = (int32_t)(arm_sin_f32(angle - 2.0f * PI / 3.0f) * HRTIM_1per4_CNT);
-        //  sin_table3[i] = (int32_t)(arm_sin_f32(angle + 2.0f * PI / 3.0f) * HRTIM_1per4_CNT);
-     }
+void sinTab_genarate() {
+  for (uint16_t i = 0; i < SIN_ALL_PIONT; i++) {
+    // 每个周期覆盖完整的2π (360°)
+    float angle = 2.0f * PI * i / SIN_ALL_PIONT;
+    sin_table1[i] = (int32_t)(arm_sin_f32(angle) * HRTIM_1per4_CNT);
+    //  sin_table2[i] = (int32_t)(arm_sin_f32(angle - 2.0f * PI / 3.0f) *
+    //  HRTIM_1per4_CNT); sin_table3[i] = (int32_t)(arm_sin_f32(angle + 2.0f *
+    //  PI / 3.0f) * HRTIM_1per4_CNT);
+  }
 }
 // static float invSqrt(float x) // 平方根倒数速算法
 // {
@@ -87,144 +87,158 @@ float Uab = 0;
 float actual_uab = 0;
 uint64_t aaa = 0;
 
-void PID_Seyduty()
-{
-    voltage_PID.ref = L_voltage_ref; // 有效值为15V
-    // voltage_PID.ref =arm_sin_f32(PI * cnt_temp / (SIN_ALL_PIONT/2)); //对有效值进行闭环
-    // Uab = 0.0047f * Uab + Uab + 0.0009f;
-    actual_uab = (collect_data->volatage[0] + collect_data->volatage[2] + collect_data->volatage[1]) *SQRT3_DIV_3;
-    pid_calculate(&voltage_PID, actual_uab);
-    // current_ref = voltage_PID.output;
-    // pid_calculate(&current_PID, current_fdb);
-    //解算
+void PID_Seyduty() {
+  voltage_PID.ref = L_voltage_ref; // 有效值为15V
+  // voltage_PID.ref =arm_sin_f32(PI * cnt_temp / (SIN_ALL_PIONT/2));
+  // //对有效值进行闭环 Uab = 0.0047f * Uab + Uab + 0.0009f;
+  actual_uab = (collect_data->volatage[0] + collect_data->volatage[2] +
+                collect_data->volatage[1]) *
+               SQRT3_DIV_3;
+  pid_calculate(&voltage_PID, actual_uab);
+  // current_ref = voltage_PID.output;
+  // pid_calculate(&current_PID, current_fdb);
+  // 解算
 }
 // static int32_t d1 = 0;
 // static int32_t d2 = 0;
 // static int32_t d3 =0;
- void duty_update()
-{
-    // svpwm_v.Uref = voltage_PID.output;
-    // svpwm_v.Uref = 0.5; //开环
-    // svpwm_calculate(&svpwm_v);
-    // 使用局部变量减少内存访问
-    // float duty_a = svpwm_v.duty_a;
-    // float duty_b = svpwm_v.duty_b;
-    // float duty_c = svpwm_v.duty_c;
-    static uint16_t cnt_temp = 0;
-    int32_t duty = (sin_table1[cnt_temp] * M_duty);
-    // int32_t duty2 = (sin_table2[cnt_temp] * M_duty);
-    // int32_t duty3 = (sin_table3[cnt_temp] * M_duty);
-    cnt_temp++;
-    if (cnt_temp >= SIN_ALL_PIONT)
-    {
-        cnt_temp = 0;
-    }
-    //单相逆变
-    // __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_E, HRTIM_COMPAREUNIT_1, HRTIM_M_HALF_CNT - HRTIM_1per4_CNT - duty);
-    // __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_E, HRTIM_COMPAREUNIT_3, HRTIM_M_HALF_CNT + HRTIM_1per4_CNT + duty);
-    // __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_F, HRTIM_COMPAREUNIT_1, HRTIM_M_HALF_CNT - HRTIM_1per4_CNT + duty);
-    // __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_F, HRTIM_COMPAREUNIT_3, HRTIM_M_HALF_CNT + HRTIM_1per4_CNT - duty);
-     __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_E, HRTIM_COMPAREUNIT_1, HRTIM_M_HALF_CNT - HRTIM_1per4_CNT);
-    __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_E, HRTIM_COMPAREUNIT_3, HRTIM_M_HALF_CNT + HRTIM_1per4_CNT);
-    __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_F, HRTIM_COMPAREUNIT_1, HRTIM_M_HALF_CNT + HRTIM_1per4_CNT);
-    __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_F, HRTIM_COMPAREUNIT_3, HRTIM_M_HALF_CNT - HRTIM_1per4_CNT );
-    //三相逆变
-    // __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_E, HRTIM_COMPAREUNIT_1, HRTIM_M_HALF_CNT - HRTIM_1per4_CNT - duty);
-    // __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_E, HRTIM_COMPAREUNIT_3, HRTIM_M_HALF_CNT + HRTIM_1per4_CNT + duty);
-    // __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_F, HRTIM_COMPAREUNIT_1, HRTIM_M_HALF_CNT - HRTIM_1per4_CNT - duty2);
-    // __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_F, HRTIM_COMPAREUNIT_3, HRTIM_M_HALF_CNT + HRTIM_1per4_CNT + duty2);
-    // __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_B, HRTIM_COMPAREUNIT_1, HRTIM_M_HALF_CNT - HRTIM_1per4_CNT - duty3);
-    // __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_B, HRTIM_COMPAREUNIT_3, HRTIM_M_HALF_CNT + HRTIM_1per4_CNT + duty3);
-    // svpwm 三相
-    // __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_E, HRTIM_COMPAREUNIT_1, HRTIM_M_HALF_CNT - HRTIM_M_HALF_CNT * duty_a);
-    // __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_E, HRTIM_COMPAREUNIT_3, HRTIM_M_HALF_CNT + HRTIM_M_HALF_CNT * duty_a);
-    // __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_F, HRTIM_COMPAREUNIT_1, HRTIM_M_HALF_CNT - HRTIM_M_HALF_CNT * duty_b);
-    // __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_F, HRTIM_COMPAREUNIT_3, HRTIM_M_HALF_CNT + HRTIM_M_HALF_CNT * duty_b);
-    // __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_B, HRTIM_COMPAREUNIT_1, HRTIM_M_HALF_CNT - HRTIM_M_HALF_CNT * duty_c);
-    // __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_B, HRTIM_COMPAREUNIT_3, HRTIM_M_HALF_CNT + HRTIM_M_HALF_CNT * duty_c);
-    // aaa++;
+void duty_update() {
+  // svpwm_v.Uref = voltage_PID.output;
+  // svpwm_v.Uref = 0.5; //开环
+  // svpwm_calculate(&svpwm_v);
+  // 使用局部变量减少内存访问
+  // float duty_a = svpwm_v.duty_a;
+  // float duty_b = svpwm_v.duty_b;
+  // float duty_c = svpwm_v.duty_c;
+  static uint16_t cnt_temp = 0;
+  int32_t duty = (sin_table1[cnt_temp] * M_duty);
+  // int32_t duty2 = (sin_table2[cnt_temp] * M_duty);
+  // int32_t duty3 = (sin_table3[cnt_temp] * M_duty);
+  cnt_temp++;
+  if (cnt_temp >= SIN_ALL_PIONT) {
+    cnt_temp = 0;
+  }
+  // 单相逆变
+  //  __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_E,
+  //  HRTIM_COMPAREUNIT_1, HRTIM_M_HALF_CNT - HRTIM_1per4_CNT - duty);
+  //  __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_E,
+  //  HRTIM_COMPAREUNIT_3, HRTIM_M_HALF_CNT + HRTIM_1per4_CNT + duty);
+  //  __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_F,
+  //  HRTIM_COMPAREUNIT_1, HRTIM_M_HALF_CNT - HRTIM_1per4_CNT + duty);
+  //  __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_F,
+  //  HRTIM_COMPAREUNIT_3, HRTIM_M_HALF_CNT + HRTIM_1per4_CNT - duty);
+  __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_E,
+                         HRTIM_COMPAREUNIT_1,
+                         HRTIM_M_HALF_CNT - HRTIM_1per4_CNT);
+  __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_E,
+                         HRTIM_COMPAREUNIT_3,
+                         HRTIM_M_HALF_CNT + HRTIM_1per4_CNT);
+  __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_F,
+                         HRTIM_COMPAREUNIT_1,
+                         HRTIM_M_HALF_CNT + HRTIM_1per4_CNT);
+  __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_F,
+                         HRTIM_COMPAREUNIT_3,
+                         HRTIM_M_HALF_CNT - HRTIM_1per4_CNT);
+  // 三相逆变
+  //  __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_E,
+  //  HRTIM_COMPAREUNIT_1, HRTIM_M_HALF_CNT - HRTIM_1per4_CNT - duty);
+  //  __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_E,
+  //  HRTIM_COMPAREUNIT_3, HRTIM_M_HALF_CNT + HRTIM_1per4_CNT + duty);
+  //  __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_F,
+  //  HRTIM_COMPAREUNIT_1, HRTIM_M_HALF_CNT - HRTIM_1per4_CNT - duty2);
+  //  __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_F,
+  //  HRTIM_COMPAREUNIT_3, HRTIM_M_HALF_CNT + HRTIM_1per4_CNT + duty2);
+  //  __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_B,
+  //  HRTIM_COMPAREUNIT_1, HRTIM_M_HALF_CNT - HRTIM_1per4_CNT - duty3);
+  //  __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_B,
+  //  HRTIM_COMPAREUNIT_3, HRTIM_M_HALF_CNT + HRTIM_1per4_CNT + duty3); svpwm
+  //  三相
+  //  __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_E,
+  //  HRTIM_COMPAREUNIT_1, HRTIM_M_HALF_CNT - HRTIM_M_HALF_CNT * duty_a);
+  //  __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_E,
+  //  HRTIM_COMPAREUNIT_3, HRTIM_M_HALF_CNT + HRTIM_M_HALF_CNT * duty_a);
+  //  __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_F,
+  //  HRTIM_COMPAREUNIT_1, HRTIM_M_HALF_CNT - HRTIM_M_HALF_CNT * duty_b);
+  //  __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_F,
+  //  HRTIM_COMPAREUNIT_3, HRTIM_M_HALF_CNT + HRTIM_M_HALF_CNT * duty_b);
+  //  __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_B,
+  //  HRTIM_COMPAREUNIT_1, HRTIM_M_HALF_CNT - HRTIM_M_HALF_CNT * duty_c);
+  //  __HAL_HRTIM_SETCOMPARE(&hhrtim1, HRTIM_TIMERINDEX_TIMER_B,
+  //  HRTIM_COMPAREUNIT_3, HRTIM_M_HALF_CNT + HRTIM_M_HALF_CNT * duty_c); aaa++;
 }
- void task_loop()
-{
-    static uint8_t is_PWD = 1;
-    static uint16_t temp_cnt = 0;
-    if(temp_cnt>2000 && is_PWD)
-    {
-        temp_cnt = 0;
-        is_PWD = 0;
-        pid_reset(&voltage_PID);
-        HAL_HRTIM_WaveformOutputStart(&hhrtim1, HRTIM_OUTPUT_TE1 | HRTIM_OUTPUT_TE2);
-        HAL_HRTIM_WaveformOutputStart(&hhrtim1, HRTIM_OUTPUT_TF1 | HRTIM_OUTPUT_TF2);
-        // HAL_HRTIM_WaveformOutputStart(&hhrtim1, HRTIM_OUTPUT_TB1 | HRTIM_OUTPUT_TB2);
-    }
-    else if(is_PWD)
-    {
-        temp_cnt++;
-        HAL_HRTIM_WaveformOutputStop(&hhrtim1, HRTIM_OUTPUT_TE1 | HRTIM_OUTPUT_TE2);
-        HAL_HRTIM_WaveformOutputStop(&hhrtim1, HRTIM_OUTPUT_TF1 | HRTIM_OUTPUT_TF2);
-        // HAL_HRTIM_WaveformOutputStop(&hhrtim1, HRTIM_OUTPUT_TB1 | HRTIM_OUTPUT_TB2);
-    }
-    // UpdateScreen();
-    // collection_update();
-    PID_Seyduty();
-    duty_update();
+void task_loop() {
+  static uint8_t is_PWD = 1;
+  static uint16_t temp_cnt = 0;
+  if (temp_cnt > 2000 && is_PWD) {
+    temp_cnt = 0;
+    is_PWD = 0;
+    pid_reset(&voltage_PID);
+    HAL_HRTIM_WaveformOutputStart(&hhrtim1,
+                                  HRTIM_OUTPUT_TE1 | HRTIM_OUTPUT_TE2);
+    HAL_HRTIM_WaveformOutputStart(&hhrtim1,
+                                  HRTIM_OUTPUT_TF1 | HRTIM_OUTPUT_TF2);
+    // HAL_HRTIM_WaveformOutputStart(&hhrtim1, HRTIM_OUTPUT_TB1 |
+    // HRTIM_OUTPUT_TB2);
+  } else if (is_PWD) {
+    temp_cnt++;
+    HAL_HRTIM_WaveformOutputStop(&hhrtim1, HRTIM_OUTPUT_TE1 | HRTIM_OUTPUT_TE2);
+    HAL_HRTIM_WaveformOutputStop(&hhrtim1, HRTIM_OUTPUT_TF1 | HRTIM_OUTPUT_TF2);
+    // HAL_HRTIM_WaveformOutputStop(&hhrtim1, HRTIM_OUTPUT_TB1 |
+    // HRTIM_OUTPUT_TB2);
+  }
+  // UpdateScreen();
+  // collection_update();
+  PID_Seyduty();
+  duty_update();
 }
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
-{
-    if (htim == &htim6) // tim6负责20khz计算
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+  if (htim == &htim6) // tim6负责20khz计算
+  {
+    task_loop(); // 任务循环
+    aaa++;
+  } else if (htim == &htim5) {
+    // 常低按下高
+    static uint8_t detect_flag = 0;
+    if (detect_flag == 0) //
     {
-        task_loop(); // 任务循环
-        aaa++;
+      if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_10) == GPIO_PIN_SET) {
+        // HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_1); // 电平反转
+        detect_flag = 1;
+      } else if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_11) ==
+                 GPIO_PIN_SET) // 读取电平是否发生变化)
+      {
+        detect_flag = 2;
+      } else if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_12) ==
+                 GPIO_PIN_SET) // 读取电平是否发生变化)
+      {
+        detect_flag = 3;
+      }
+    } else {
+      if ((HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_10) == GPIO_PIN_RESET) &&
+          detect_flag == 1) // 读取电平是否发生变化
+      {
+        detect_flag = 0;
+        change_freq(&svpwm_v, svpwm_v.target_freq + 1);
+      } else if ((HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_11) == GPIO_PIN_RESET) &&
+                 detect_flag == 2) {
+        detect_flag = 0;
+        change_freq(&svpwm_v, svpwm_v.target_freq - 1);
+      } else if ((HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_12) == GPIO_PIN_RESET) &&
+                 detect_flag == 3) {
+        detect_flag = 0;
+        // change_freq(&svpwm_v, 50);
+      }
     }
-    else if(htim==&htim5) 
-    {
-        //常低按下高
-        static uint8_t detect_flag = 0;
-        if (detect_flag == 0) //
-        {
-            if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_10) == GPIO_PIN_SET)
-            {
-                // HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_1); // 电平反转
-                detect_flag = 1;
-            }
-            else if ( HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_11) == GPIO_PIN_SET) // 读取电平是否发生变化)
-            {
-                detect_flag = 2;
-            }
-            else if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_12) == GPIO_PIN_SET) // 读取电平是否发生变化)
-            {
-                detect_flag = 3;
-            }
-        }
-        else
-        {
-            if ((HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_10) == GPIO_PIN_RESET)&& detect_flag ==1) // 读取电平是否发生变化
-            {
-                detect_flag = 0;
-                change_freq(&svpwm_v, svpwm_v.target_freq+1);
-            }
-            else if ((HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_11) == GPIO_PIN_RESET) && detect_flag == 2)
-            {
-                detect_flag = 0;
-                change_freq(&svpwm_v, svpwm_v.target_freq - 1);
-            }
-            else if ((HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_12) == GPIO_PIN_RESET) && detect_flag == 3)
-            {
-                detect_flag = 0;
-                // change_freq(&svpwm_v, 50);
-            }
-        }
-    }
+  }
 }
-void show_screen()
-{
-    OLED_ShowNum(32, 0, svpwm_v.target_freq, 3, OLED_8X16);
-    OLED_ShowFloatNum(32, 16, collect_data->volatage[0], 3, 2, OLED_8X16);
-    OLED_ShowFloatNum(32, 32, collect_data->volatage[1], 3, 2, OLED_8X16);
-    OLED_ShowFloatNum(32, 48, collect_data->volatage[2], 3, 2, OLED_8X16);
-    // OLED_Printf(64, 0, OLED_8X16, "%dHz", svpwm_v.target_freq);
-    // OLED_Printf(64, 16, OLED_8X16, "%.2fV", collect_data->volatage[0]);
-    // OLED_Printf(64, 32, OLED_8X16, "%.2fV", collect_data->volatage[1]);
-    // OLED_Printf(64, 48, OLED_8X16, "%.2fV", collect_data->volatage[2]);
-    OLED_Update();
+void show_screen() {
+  OLED_ShowNum(32, 0, svpwm_v.target_freq, 3, OLED_8X16);
+  OLED_ShowFloatNum(32, 16, collect_data->volatage[0], 3, 2, OLED_8X16);
+  OLED_ShowFloatNum(32, 32, collect_data->volatage[1], 3, 2, OLED_8X16);
+  OLED_ShowFloatNum(32, 48, collect_data->volatage[2], 3, 2, OLED_8X16);
+  // OLED_Printf(64, 0, OLED_8X16, "%dHz", svpwm_v.target_freq);
+  // OLED_Printf(64, 16, OLED_8X16, "%.2fV", collect_data->volatage[0]);
+  // OLED_Printf(64, 32, OLED_8X16, "%.2fV", collect_data->volatage[1]);
+  // OLED_Printf(64, 48, OLED_8X16, "%.2fV", collect_data->volatage[2]);
+  OLED_Update();
 }
